5ee696df9d6f6a77c8586ed0c01240849f40b1bf
rishabhkeshan <rishabhkeshan@gmail.com>
2023-07-12 18:00:21 +0530

    merged wallet and challenge 1 data

---
+++ frontend/src/app/walletconnect/walletconnect.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WalletconnectComponent } from './walletconnect.component';
+
+describe('WalletconnectComponent', () => {
+  let component: WalletconnectComponent;
+  let fixture: ComponentFixture<WalletconnectComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WalletconnectComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WalletconnectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
